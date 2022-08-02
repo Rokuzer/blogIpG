d76ba2d173fe52112f40e734b08790ef89e1ae01
Rokuzer <ruben.dellave@outlook.com>
2022-08-02 19:09:10 +0200

    Algo de diseño CSS y Boostrap

--- blogIpG/src/Form/Type/BlogType.php
+++ blogIpG/src/Form/Type/BlogType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class BlogType extends AbstractType
 {
@@ -30,5 +31,6 @@ class BlogType extends AbstractType
         ]);
         $builder->add('title', TextType::class, ['required' => true]);
         $builder->add('body', TextareaType::class, ['required' => true]);
+        $builder->add('Create', SubmitType::class);
     }
 }
